216ae8ad99edf1aa3a68c5e747420ee53545ce58
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-05-29 18:27:46 +0530

    statement

---
+++ src/com/basic/statement.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class statement {
+	public static void main(String args[]) {
+	 int i=3;	
+     if(i % 2==0)
+    	 System.out.println("even");
+     else
+    	 System.out.println("odd");
+}
+}
+
