dbb4dda32487cc888183cb2f96501c07a9a91d23
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-05-29 18:16:51 +0530

    operators

---
+++ src/com/basic/operators.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class operators {
+	public static void main(String args[]) {
+		int count1=7;
+		int count2=8;
+		System.out.println(count1>count2);
+		System.out.println(count1<count2);
+		System.out.println(count1>=count2);
+		System.out.println(count1<=count2);
+		System.out.println(count1==count2);
+	    System.out.println(count1&count2);
+		System.out.println(count1|count2);
+		System.out.println(count1^count2);
+		
+		
+		
+			
+		}
+
+	}
+
+
+
