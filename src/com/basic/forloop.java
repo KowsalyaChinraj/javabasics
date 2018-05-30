cd15d7559a1c7907a2df778bd1a3f0a5cdfa5f67
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-05-30 10:13:57 +0530

    forloop program

---
+++ src/com/basic/forloop.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class forloop {
+	public static void main(String args[]) {
+		int i=0;
+		for(i=0;i<10;i++)
+		{
+			System.out.println("i= "+i);
+		    if(i==4) {
+		         break;
+		    }
+		
+		}
+	}
+
+}
