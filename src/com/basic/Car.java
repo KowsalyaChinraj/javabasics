de1e42b30b4248e2804f69f9bb4d457a491bea32
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-05-30 18:05:10 +0530

    object sample program

---
+++ src/com/basic/Car.java
@@ -0,0 +1,46 @@
+package com.basic;
+
+public class Car {
+	String model;
+	double speed;
+	public Car() {
+	}
+	
+	
+	public String getModel() {
+		return model;
+	}
+
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+	public double getSpeed() {
+		return speed;
+	}
+
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+
+	public boolean start() {
+		return true;
+		
+	}
+	public void accelerate() {
+		
+	}
+	public static void main(String args[]) {
+		Car car1=new Car();
+		Car car2=new Car();
+		car1.speed=20;
+		car2.speed=30;
+		System.out.println(car1.speed);
+		System.out.println(car2.speed);
+
+}
+}
