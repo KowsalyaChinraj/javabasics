3022bf75c29201ab8c9c4c8b6f351006f2aee354
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-05-30 09:40:49 +0530

    switchcase

---
+++ src/com/basic/switchcase.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class switchcase {
+	public static void main(String args[]) {
+		int choice=30;
+		switch(choice) {
+		case 10:
+			System.out.println(34);
+			break;
+		case 20:
+			System.out.println(23);
+			break;
+		case 30:
+			System.out.println(56);
+			break;
+		case 40:
+			System.out.println(45);
+			break;
+		
+		}
+		
+	}
+
+}
