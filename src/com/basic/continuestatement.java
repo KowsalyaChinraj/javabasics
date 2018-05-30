d1db06b48c1fbfa1d888a36dc10abd150e5871d3
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-05-30 10:37:37 +0530

    continue statement

---
+++ src/com/basic/continuestatement.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class continuestatement {
+	public static void main(String args[]) {
+		int i=0;
+		for(i=0;i<10;i++)
+		{
+		if(i==3) {
+			continue;
+		}
+		System.out.println("i="+i);
+	}
+
+}
+
+}
