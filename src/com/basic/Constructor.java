7393ed31380c4a5d190ce3f974df3058b3e299e7
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-06-09 12:00:24 +0530

    constructor sample program

---
+++ src/com/basic/Constructor.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Constructor {
+	
+	public Constructor() {
+		System.out.println("constructor called");
+		
+	}
+public static void main(String args[]) {
+	Constructor constructor=new Constructor();
+	
+	
+}
+}
