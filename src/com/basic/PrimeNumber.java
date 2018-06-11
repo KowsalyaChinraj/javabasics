1bd9cf82162e5218d94a71d81b07bf5e8f1ca5ba
KowsalyaChinraj <ckowsalya97@gmail.com>
2018-06-11 12:44:16 +0530

    prime numbers

---
+++ src/com/basic/PrimeNumber.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class PrimeNumber {
+	
+	public static void main(String args[]) {
+		int i,number=9;
+
+
+		for(i=1;i<=9;i++) {
+			if(number%i==0) {
+				
+			
+			
+				System.out.println("Prime number");
+			}
+			else {
+				System.out.println("not a Prime number");
+				
+			}
+		}
+	}
+
+}
